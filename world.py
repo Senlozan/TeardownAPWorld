1596b9c762e873572f35ebb8a7ab98efd618fcbf
Senlozan <disciaordain@gmail.com>
2025-12-05 18:43:05 -0800

    Create world.py

---
+++ world.py
@@ -0,0 +1,8 @@
+from collections.abc import Mapping
+from typing import Any
+
+# Imports of base Archipelago modules must be absolute.
+from worlds.AutoWorld import World
+
+# Imports of your world's files must be relative.
+from . import items, locations, options, regions
